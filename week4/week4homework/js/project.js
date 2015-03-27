a24a136368c845288a40b24a66c4062187c5ed82
vvsanders <valentinesanders@valentinesmbp2.home>
2015-03-26 23:11:13 -0400

    Final submission

--- week4/week4homework/js/project.js
+++ week4/week4homework/js/project.js
@@ -1,18 +1,17 @@
-var red = document.getElementById("red").value;
-var green = document.getElementById("green").value;
-var blue = document.getElementById("blue").value;
-var myColor = "rgb(" +red+ "," +green+ "," +blue+ ")"
-
-
 function changeColor() {
+	var red = document.getElementById("red").value;
+	var green = document.getElementById("green").value;
+	var blue = document.getElementById("blue").value;
+	var myColor = "rgb(" +red+ "," +green+ "," +blue+ ")"
 
-	var myColor = document.getElementById("rgb").value;
-
+	document.getElementById("colorfultext").value = myColor;
 	document.getElementById("colorfultext").style.backgroundColor = myColor;
 	document.getElementById("colorfultext").innerHTML = myColor;
+	document.body.style.backgroundColor = myColor;
 }
 
-
-
-
 document.getElementById("colorbutton").onclick = changeColor;
+
+
+
+
