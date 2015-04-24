e63d39680215b72b5c154b60ab3333841c7f8a24
vvsanders <valentinemsanders@gmail.com>
2015-04-24 17:45:50 -0400

    test

---
+++ wine wip/Wine/js/project.js
@@ -0,0 +1,94 @@
+// $('#musky').mouseover(function() {
+//     $('body').css("background-color","#d66554");
+// });
+
+// $('#earthy').mouseover(function() {
+//     $('body').css("background-color","#944337");
+// });
+
+// $("#smokey").mouseover(function() {
+//     $('body').css("background-color","#ce4834");
+// });
+
+// $('#accessible').mouseover(function() {
+//     $('body').css("background-color","#dc7c6e");
+// });
+
+// $('#clean').mouseover(function() {
+//     $('body').css("background-color","#ff745f");
+// });
+
+// $('#delicate').mouseover(function() {
+//     $('body').css("background-color","#ffb6cb");
+// });
+
+// $('#fullbodied').mouseover(function() {
+//     $('body').css("background-color","#511231");
+// });
+
+// $('#dense').mouseover(function() {
+//     $('body').css("background-color","#430725");
+// });
+
+// $('#spicy').mouseover(function() {
+//     $('body').css("background-color","#822323");
+// });
+
+// $('#nutty').mouseover(function() {
+//     $('body').css("background-color","#730505");
+// });
+
+// $('#buttery').mouseover(function() {
+//     $('body').css("background-color","#732605");
+// });
+
+// $('li').mouseout(function() {
+//     $('body').css("background-color","white");
+// });
+
+
+//
+
+$('#sparkling a').mouseover(function() {
+    $('body').css("background-color","#dfdeae");
+});
+
+$('#drywhite').mouseover(function() {
+    $('body').css("background-color","#dedfa3");
+});
+
+$("#sweet").mouseover(function() {
+    $('body').css("background-color","#eed47b");
+});
+
+$('#rich').mouseover(function() {
+    $('body').css("background-color","#fbcf69");
+});
+
+$('#rose').mouseover(function() {
+    $('body').css("background-color","#ffb6cb");
+});
+
+$('#lightred').mouseover(function() {
+    $('body').css("background-color","#b51d39");
+});
+
+$('#mediumred').mouseover(function() {
+    $('body').css("background-color","#7d0c2a");
+});
+
+$('#boldred').mouseover(function() {
+    $('body').css("background-color","#430725");
+});
+
+$('#dessert').mouseover(function() {
+    $('body').css("background-color","#822323");
+});
+
+$('li').mouseout(function() {
+    $('body').css("background-color","white");
+});
+
+
+
+
