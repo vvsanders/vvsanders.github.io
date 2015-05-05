780f451d9dfb4673a2ced608c2608251637431df
vvsanders <valentinesanders@valentinesmbp2.home>
2015-05-04 23:43:59 -0400

    second submission

--- winetally/js/project.js
+++ winetally/js/project.js
@@ -19,7 +19,7 @@ $( document ).ready(function() {
         $('p').css('visibility', 'hidden');
     });
 
-    $('#sparkling').mouseover(function() {
+    $('.sparkling').mouseover(function() {
         $('body').css("background-color","#dfdeae");
     });
 
@@ -34,45 +34,62 @@ $( document ).ready(function() {
         $('body').css("background-color", "white");
         }
         if($(window).width() <= 640) {
-            $("#sparkling a").css("color", "#dfdeae");
+            $(".sparkling a").css("color", "#dfdeae");
         }
     });
 
 
-    $('#drywhite').mouseover(function() {
+    $('.drywhite').mouseover(function() {
         $('body').css("background-color","#dedfa3");
     });
 
-    $("#sweet").mouseover(function() {
+    $(".sweet").mouseover(function() {
         $('body').css("background-color","#eed47b");
     });
 
-    $('#rich').mouseover(function() {
+    $('.richwhite').mouseover(function() {
         $('body').css("background-color","#fbcf69");
     });
 
-    $('#rose').mouseover(function() {
+    $('.rose').mouseover(function() {
         $('body').css("background-color","#ffb6cb");
     });
 
-    $('#lightred').mouseover(function() {
+    $('.lightred').mouseover(function() {
         $('body').css("background-color","#b51d39");
     });
 
-    $('#mediumred').mouseover(function() {
+    $('.mediumred').mouseover(function() {
         $('body').css("background-color","#7d0c2a");
     });
 
-    $('#boldred').mouseover(function() {
+    $('.boldred').mouseover(function() {
         $('body').css("background-color","#430725");
     });
 
-    $('#dessert').mouseover(function() {
+    $('.dessert').mouseover(function() {
         $('body').css("background-color","#822323");
+    });
+
+    $('.mediumbold').mouseover(function() {
+        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#7d0c2a), to(#430725))");
+    });
+
+    $('.dessertsparkling').mouseover(function() {
+        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#822323), to(#dfdeae))");
+    });
+
+    $('.dryred').mouseover(function() {
+        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#dedfa3), to(#7d0c2a))");
+    });
+
+    $('.richlight').mouseover(function() {
+        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#fbcf69), to(#b51d39))");
     });
 
     $('li').mouseout(function() {
         $('body').css("background-color","white");
+        $('body').css("background-image", "none");
     });
 
 });
