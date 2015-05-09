9cff54c9959432fe239cd310125a8072a57df240
vvsanders <valentinesanders@Valentines-MacBook-Pro-2.local>
2015-05-09 12:49:25 -0400

    final submission

--- winetally/js/project.js
+++ winetally/js/project.js
@@ -1,15 +1,3 @@
-// $('#sparkling a').on({
-//     mouseover: function(){
-//         $('body').css({background: '#dfdeae'});
-//     },
-//      mouseleave: function(){
-//         $('body').css({background: 'white'});
-//     },
-//     click: function(){
-//         $(this).off('mouseleave');
-//     }
-
-// });
 $( document ).ready(function() {
     $('#about').mouseover(function() {
         $('p').css('visibility', 'visible');
@@ -19,77 +7,62 @@ $( document ).ready(function() {
         $('p').css('visibility', 'hidden');
     });
 
-    $('.sparkling').mouseover(function() {
-        $('body').css("background-color","#dfdeae");
-    });
 
-    // $(window).resize(function(){
-    //     if($(window).width() <= 640) {
-    //         $("#sparkling a").css("color", "#dfdeae");
-    //     }
-    // }); 
+    // Mouse over
+    $('#main a').mouseover(function() {
+        var color = '#' + $(this).attr('id');
 
-    $(window).resize(function(){
-        if ($(window).width() <= 640){
-        $('body').css("background-color", "white");
-        }
-        if($(window).width() <= 640) {
-            $(".sparkling a").css("color", "#dfdeae");
+        if ($(window).width() > 640) {
+            $('body').css('background-color', color);
+            $(this).css('color', "white");
+        } else {
+            $(this).css('color', color);
         }
     });
 
+      $('#foodlist a').mouseover(function() {
+        var color = '#' + $(this).attr('id');
 
-    $('.drywhite').mouseover(function() {
-        $('body').css("background-color","#dedfa3");
+        if ($(window).width() > 640) {
+            $('body').css('background-color', color);
+            $(this).css('color', "white");
+        } else {
+            $(this).css('color', color);
+        }
     });
 
-    $(".sweet").mouseover(function() {
-        $('body').css("background-color","#eed47b");
+    // Mouse out    
+    $('#main a').mouseout(function() {
+         var color = '#' + $(this).attr('id');
+
+        if ($(window).width() > 640) {
+            $('body').css('background-color', 'white');
+            $(this).css('color', 'black');
+        } else {
+            $(this).css('color', color);
+        }
+
     });
 
-    $('.richwhite').mouseover(function() {
-        $('body').css("background-color","#fbcf69");
+    $('#foodlist a').mouseout(function() {
+         var color = '#' + $(this).attr('id');
+
+        if ($(window).width() > 640) {
+            $('body').css('background-color', 'white');
+            $(this).css('color', 'black');
+        } else {
+            $(this).css('color', color);
+        }
+
     });
 
-    $('.rose').mouseover(function() {
-        $('body').css("background-color","#ffb6cb");
+
+    $('#white').toggle(function() {
+        $(this).toggleClass("ul",".whites");
     });
 
-    $('.lightred').mouseover(function() {
-        $('body').css("background-color","#b51d39");
-    });
-
-    $('.mediumred').mouseover(function() {
-        $('body').css("background-color","#7d0c2a");
-    });
-
-    $('.boldred').mouseover(function() {
-        $('body').css("background-color","#430725");
-    });
-
-    $('.dessert').mouseover(function() {
-        $('body').css("background-color","#822323");
-    });
-
-    $('.mediumbold').mouseover(function() {
-        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#7d0c2a), to(#430725))");
-    });
-
-    $('.dessertsparkling').mouseover(function() {
-        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#822323), to(#dfdeae))");
-    });
-
-    $('.dryred').mouseover(function() {
-        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#dedfa3), to(#7d0c2a))");
-    });
-
-    $('.richlight').mouseover(function() {
-        $('body').css("background-image","-webkit-gradient(linear, left top, left bottom, from(#fbcf69), to(#b51d39))");
-    });
-
-    $('li').mouseout(function() {
-        $('body').css("background-color","white");
-        $('body').css("background-image", "none");
+     $('#red').toggle(function() {
+        $(this).toggleClass("ul",".red");
     });
 
 });
